57abda30179aece0d5aec30d917eddba37a50602
jonasgaribay0 <jonasgaribay012@gmail.com>
2022-02-25 11:12:16 -0800

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.util.Keyboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import static frc.robot.util.Keyboard.Keys.*;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  // Board does not need port number, this also means it cannot differentiate between different keyboards
+  private final Keyboard board = new Keyboard();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    // You have to refer to the keys from the board since the keys are declared in the Keyboard class
+    // It is pretty much the same as using a Joystick button
+    board.keys[A_].whenPressed(
+      () -> {
+
+        SmartDashboard.putBoolean("Keyboard: A", board.keys[A_].get());
+
+      }
+    )
+    .whenReleased(
+      () -> {
+
+        SmartDashboard.putBoolean("Keyboard: A", board.keys[A_].get());
+
+      }
+    );
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return null;
+  }
+}
