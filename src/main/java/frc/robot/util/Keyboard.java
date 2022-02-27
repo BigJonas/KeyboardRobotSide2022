4796036677a4cbd595e915e76962656da9133aef
jonasgaribay0 <jonasgaribay012@gmail.com>
2022-02-26 16:44:56 -0800

    Now works with desktop client

--- src/main/java/frc/robot/util/Keyboard.java
+++ src/main/java/frc/robot/util/Keyboard.java
@@ -4,89 +4,94 @@
 
 package frc.robot.util;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import static frc.robot.Constants.Keyboard.*;
+import static frc.robot.util.Keyboard.Keys.*;
+import static java.awt.event.KeyEvent.*;
+
+import edu.wpi.first.networktables.*;
 
 /** Add your docs here. */
-public class Keyboard 
-    implements KeyListener{
+public class Keyboard {
 
-    // Kind of irritating to change so I hope I dont have to do that
     public class Keys {
-        public static final int SC_ = 0;
-        public static final int EQ_ = 1;
+        public static final int firstKeyOffSet = VK_SEMICOLON;
 
-        public static final int A_ = 2;
-        public static final int B_ = 3;
-        public static final int C_ = 4;
-        public static final int D_ = 5;
-        public static final int E_ = 6;
-        public static final int F_ = 7;
-        public static final int G_ = 8;
-        public static final int H_ = 9;
-        public static final int I_ = 10;
-        public static final int J_ = 11;
-        public static final int K_ = 12;
-        public static final int L_ = 13;
-        public static final int M_ = 14;
-        public static final int N_ = 15;
-        public static final int O_ = 16;
-        public static final int P_ = 17;
-        public static final int Q_ = 18;
-        public static final int R_ = 19;
-        public static final int S_ = 20;
-        public static final int T_ = 21;
-        public static final int U_ = 22;
-        public static final int V_ = 23;
-        public static final int W_ = 24;
-        public static final int X_ = 25;
-        public static final int Y_ = 26;
-        public static final int Z_ = 27;
+        public static final int SC_ = VK_SEMICOLON - firstKeyOffSet;
+        public static final int EQ_ = VK_EQUALS - firstKeyOffSet;
+
+        public static final int A_ = VK_A - firstKeyOffSet;
+        public static final int B_ = VK_B - firstKeyOffSet;
+        public static final int C_ = VK_C - firstKeyOffSet;
+        public static final int D_ = VK_D - firstKeyOffSet;
+        public static final int E_ = VK_E - firstKeyOffSet;
+        public static final int F_ = VK_F - firstKeyOffSet;
+        public static final int G_ = VK_G - firstKeyOffSet;
+        public static final int H_ = VK_H - firstKeyOffSet;
+        public static final int I_ = VK_I - firstKeyOffSet;
+        public static final int J_ = VK_J - firstKeyOffSet;
+        public static final int K_ = VK_K - firstKeyOffSet;
+        public static final int L_ = VK_L - firstKeyOffSet;
+        public static final int M_ = VK_M - firstKeyOffSet;
+        public static final int N_ = VK_N - firstKeyOffSet;
+        public static final int O_ = VK_O - firstKeyOffSet;
+        public static final int P_ = VK_P - firstKeyOffSet;
+        public static final int Q_ = VK_Q - firstKeyOffSet;
+        public static final int R_ = VK_R - firstKeyOffSet;
+        public static final int S_ = VK_S - firstKeyOffSet;
+        public static final int T_ = VK_T - firstKeyOffSet;
+        public static final int U_ = VK_U - firstKeyOffSet;
+        public static final int V_ = VK_V - firstKeyOffSet;
+        public static final int W_ = VK_W - firstKeyOffSet;
+        public static final int X_ = VK_X - firstKeyOffSet;
+        public static final int Y_ = VK_Y - firstKeyOffSet;
+        public static final int Z_ = VK_Z - firstKeyOffSet;
 
     }
 
-    // Is the first value in KeyEvent that is used to offset the array of keys
-    private int firstKeyOffset;
+    public static NetworkTableInstance inst;
+    public static NetworkTable table;
+    public static NetworkTableEntry keysEntries[];
 
     // An array of the keys you can use
     public Key keys[];
 
     public Keyboard() {
+        configurateNetworkTable();
+        
+        keys = new Key[KEY_AMOUNT];
 
-        firstKeyOffset = KeyEvent.VK_SEMICOLON;
-
-        keys = new Key[28];
-
-        // Initilizing the keys from A-Z
+        // Initilizing the keys from ;-Z
         for (int i = 0; i < keys.length; i++) {
             keys[i] = new Key(this, i);
         }
 
     }
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        
-        keys[e.getKeyCode() - firstKeyOffset].isPressed = true;
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        
-        keys[e.getKeyCode() - firstKeyOffset].isPressed = false;
-        
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // Do nothing
-        
-    }
-
     public boolean getKeyPressed(int id) {
 
         return keys[id].isPressed;
+
+    }
+
+    private void configurateNetworkTable() {
+        inst = NetworkTableInstance.getDefault();
+        table = inst.getTable("keyboard");
+        keysEntries = new NetworkTableEntry[KEY_AMOUNT];
+        
+        // Declare keys used this way
+        keysEntries[A_] = table.getEntry("a");
+
+        // Declare key listener
+        keysEntries[A_].addListener(
+            event -> {
+                this.keys[A_].isPressed = keysEntries[A_].getBoolean(false);
+            }, 
+            
+            EntryListenerFlags.kNew | EntryListenerFlags.kUpdate
+
+        );
+
+        inst.startClientTeam(2557);
 
     }
 }
